8a7e86cad65c565c3a505da0cb5571182dc83ebd
NtyamK <ntyamkevin.contact@gmail.com>
2025-01-13 10:45:28 +0100

    Ajout des dépendances Amazon. 1/2
    Il faut compiler nous-même les dépendances serveurs. Le script autoload Gamelift ne fonctionnera pas sans.

--- Scripts/DEBUG/Matchmaking/Matchmaking.cs
+++ Scripts/DEBUG/Matchmaking/Matchmaking.cs
@@ -1,15 +1,9 @@
 using Godot;
 using System;
-using PlayFab;
-using PlayFab.ServerModels;
-using PlayFab.ClientModels;
+
 
 public partial class Matchmaking : Node2D
 {
-	// Playfab
-	private string playFabTitleId = "7882E";  // Get from PlayFab Game Manager
-    private string developerSecretKey = "YING8THDD6GCIYS86CCMHHRMCP3SEJJR8SE4RYQ51EY97RPRUN";  // Get from PlayFab Game Manager
-
 	// SERVER PARAMETERS
 	const int SERVER_PORT = 8080;
 	const string SERVER_IP = "127.0.0.1";
@@ -21,18 +15,8 @@ public partial class Matchmaking : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		PlayFabSettings.staticSettings.TitleId = playFabTitleId; // Set PlayFab title
-        PlayFabSettings.staticSettings.DeveloperSecretKey = developerSecretKey;  // Set your secret key
-	}
-	private void OnLoginSuccess(LoginResult result)
-    {
-        GD.Print("Login successful!");
-    }
 
-    private void OnLoginError(PlayFabError error)
-    {
-        GD.Print("Login failed: " + error.ErrorMessage);
-    }
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -44,7 +28,6 @@ public partial class Matchmaking : Node2D
 	{
 		UI.Hide();
 		BecomeClient();
-		LoginClient();
 	}
 
 	private void _on_host_match_button_pressed()
@@ -77,37 +60,6 @@ public partial class Matchmaking : Node2D
 		MultiplayerApi Multiplayer = GetTree().GetMultiplayer();
 
 		Multiplayer.MultiplayerPeer = ClientPeer;
-	}
-
-	private async void LoginClient()
-	{
-		LoginWithEmailAddressRequest request = new LoginWithEmailAddressRequest
-		{
-			Email = "zacharia@mesaeret.com",
-			Password = "123456"
-		};
-
-		var userLogin = PlayFabClientAPI.LoginWithEmailAddressAsync(request);
-		PlayFabResult<LoginResult> loginCredentials = await userLogin;
-		Rpc("ServerAuthentificateLogin", loginCredentials.Result.PlayFabId);
-	}
-
-	[Rpc(MultiplayerApi.RpcMode.AnyPeer)]
-	private async void ServerAuthentificateLogin(string playerId)
-	{
-		AuthenticateSessionTicketRequest request = new AuthenticateSessionTicketRequest
-		{
-			SessionTicket = playerId
-		};
-		var ticketAuth = PlayFabServerAPI.AuthenticateSessionTicketAsync(request);
-		var newAuth = await ticketAuth;
-
-		if (newAuth != null)
-		{
-			GD.Print("Login successful");
-		}else{
-			GD.Print("Login failed");
-		}
 	}
 
 	private void OnPeerDisconnected(long id)
